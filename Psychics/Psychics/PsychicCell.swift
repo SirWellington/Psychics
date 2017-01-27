6bafc6996567278d6e15f209a61a52bee4a9388b
Wellington Moreno <wellington@blacksource.tech>
2017-01-27 15:29:31 -0800

    Connects storyboard with cell
    + Removes unused ViewController

---
+++ Psychics/Psychics/PsychicCell.swift
@@ -0,0 +1,19 @@
+//
+//  PsychicCell.swift
+//  Psychics
+//
+//  Created by Wellington Moreno on 1/27/17.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PsychicCell: UITableViewCell {
+    
+    @IBOutlet weak var psychicCover: UIImageView!
+    @IBOutlet weak var psychicName: UILabel!
+    @IBOutlet weak var psychicPrice: UILabel!
+    @IBOutlet weak var specialties: UILabel!
+    @IBOutlet weak var skills: UILabel!
+}
