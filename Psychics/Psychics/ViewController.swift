97bdffcc656330b9aa98f43f0c497e5585a22d83
Wellington Moreno <wellington@blacksource.tech>
2017-01-27 14:37:53 -0800

    Adds Xcode project

---
+++ Psychics/Psychics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychics
+//
+//  Created by Wellington Moreno on 1/27/17.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
